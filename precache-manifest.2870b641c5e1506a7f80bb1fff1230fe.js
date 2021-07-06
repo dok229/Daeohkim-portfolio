f9839ecdd3f4c40a132b67b8dd0c3237af916503
unknown <daeohkimchi@gmail.com>
2021-07-07 03:40:50 +0900

    Updates

---
+++ precache-manifest.2870b641c5e1506a7f80bb1fff1230fe.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "788179ffd864203f4b13220283544965",
+    "url": "/Daeohkim-portfolio/index.html"
+  },
+  {
+    "revision": "4a73d5ed24543d971909",
+    "url": "/Daeohkim-portfolio/static/css/2.14351861.chunk.css"
+  },
+  {
+    "revision": "6b6400eb7520861e862a",
+    "url": "/Daeohkim-portfolio/static/css/main.08240a24.chunk.css"
+  },
+  {
+    "revision": "4a73d5ed24543d971909",
+    "url": "/Daeohkim-portfolio/static/js/2.d660f6e6.chunk.js"
+  },
+  {
+    "revision": "76c33606446526c6ec38bc107ec65a87",
+    "url": "/Daeohkim-portfolio/static/js/2.d660f6e6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6b6400eb7520861e862a",
+    "url": "/Daeohkim-portfolio/static/js/main.a441fc43.chunk.js"
+  },
+  {
+    "revision": "465686b2330fbdaeb168",
+    "url": "/Daeohkim-portfolio/static/js/runtime-main.e9fa9940.js"
+  },
+  {
+    "revision": "618cedb9175eaecb2a2367dbe25d886e",
+    "url": "/Daeohkim-portfolio/static/media/Hello.618cedb9.jpg"
+  },
+  {
+    "revision": "3bfcf1323a33263323e2e833cb19c6d8",
+    "url": "/Daeohkim-portfolio/static/media/Hello2.3bfcf132.jpg"
+  },
+  {
+    "revision": "21218b73d634fe532e4ac92d2cd00960",
+    "url": "/Daeohkim-portfolio/static/media/My-logo4.21218b73.ico"
+  },
+  {
+    "revision": "e7d2d6912f3448b008b34a091be72fc9",
+    "url": "/Daeohkim-portfolio/static/media/My-logo4.e7d2d691.jpg"
+  },
+  {
+    "revision": "a8bec2143f76aca5c3303429cef54708",
+    "url": "/Daeohkim-portfolio/static/media/project1.a8bec214.gif"
+  },
+  {
+    "revision": "d7205d64cb901681cfb61de36b69f41a",
+    "url": "/Daeohkim-portfolio/static/media/project2.d7205d64.gif"
+  },
+  {
+    "revision": "25c72ad77c0422410306aaca6e28bfc9",
+    "url": "/Daeohkim-portfolio/static/media/project3.25c72ad7.gif"
+  },
+  {
+    "revision": "d0b8c7aa4988bd185a928386f3a669ef",
+    "url": "/Daeohkim-portfolio/static/media/workshop.d0b8c7aa.jpg"
+  }
+]);
